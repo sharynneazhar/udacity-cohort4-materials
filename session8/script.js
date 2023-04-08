47fb71a6714ed4bb04ce9f369dab455ed1321798
Sharynne Azhar <sharynneazhar@gmail.com>
2023-04-07 19:07:39 -0500

    upload session 8

---
+++ session8/script.js
@@ -0,0 +1,63 @@
+// ## Data Types
+// For each expression, predict what you think the output will be in a comment (`//`) ***without first running the command***. Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+// You may run these commands inside the Dev Tool console to double check your answer!
+
+// #### Example
+
+typeof '15';
+// Prediction: Number
+// Actual: String
+
+// What is the output of each of the expressions below?
+
+typeof 15;
+// Prediction:
+// Actual:
+
+typeof 5.5;
+// Prediction:
+// Actual:
+
+typeof NaN;
+// Prediction:
+// Actual:
+
+typeof 'hello';
+// Prediction:
+// Actual:
+
+typeof true;
+// Prediction:
+// Actual:
+
+typeof (1 != 2);
+// Prediction:
+// Actual:
+
+'hamburger' + 's';
+// Prediction:
+// Actual:
+
+'hamburgers' - 's';
+// Prediction:
+// Actual:
+
+'1' + '3';
+// Prediction:
+// Actual:
+
+'1' - '3';
+// Prediction:
+// Actual:
+
+'johnny' + 5;
+// Prediction:
+// Actual:
+
+'johnny' - 5;
+// Prediction:
+// Actual:
+
+99 * 'luftbaloons';
+// Prediction:
+// Actual:
